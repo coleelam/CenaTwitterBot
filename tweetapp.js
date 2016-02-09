2c2fd555306dbe6acf2408964341f6458c2fc102
coleelam <elamc@purdue.edu>
2016-02-09 01:04:07 -0500

    messing with code

--- tweetapp.js
+++ tweetapp.js
@@ -6,16 +6,13 @@ app.listen(process.env.PORT || 5000);
 // END HEROKU SETUP
 
 var config = {
-    me: 'cole_elam',
-    keys: {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    },
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-var Twitter = require('twitter')(config.keys);
+var Twitter = require('twitter')(config);
 
 var cenaArray = [
     "Your time is up!",
